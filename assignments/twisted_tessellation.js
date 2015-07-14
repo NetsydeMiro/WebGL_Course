880f6b35e6401af9de2d8bf606cd3d9464874b70
NetsydeMiro <miro@netsyde.com>
2015-07-13 20:07:51 -0400

    altered mesh so that rendering happens in one go in gpu

--- assignments/twisted_tessellation.js
+++ assignments/twisted_tessellation.js
@@ -2,6 +2,7 @@
 
 var canvas, gl, bufferId;
 var points = [];
+var lines = [];
 var maxSubdivisions = 10;
 var maxRotation = 360;
 var maxConstant = 0.05 
@@ -11,69 +12,69 @@ var vColor;
 
 function init()
 {
-    canvas = document.getElementById( "gl-canvas" );
+  canvas = document.getElementById( "gl-canvas" );
 
-    canvas.width = canvas.scrollWidth;
-    canvas.height = canvas.scrollWidth;
+  canvas.width = canvas.scrollWidth;
+  canvas.height = canvas.scrollWidth;
 
-    // TODO: use a mvc framework to track variable states, and 
-    // use inputs so that manual numbers can be entered (rather than just spans to display them)
-    document.getElementById( "inSubdivisions" ).max = 
+  // TODO: use a mvc framework to track variable states, and 
+  // use inputs so that manual numbers can be entered (rather than just spans to display them)
+  document.getElementById( "inSubdivisions" ).max = 
     document.getElementById( "maxSubdivisions" ).innerHTML = 
     maxSubdivisions;
 
-    document.getElementById( "inRotation" ).max = 
+  document.getElementById( "inRotation" ).max = 
     document.getElementById( "maxRotation" ).innerHTML = 
     maxRotation;
 
-    document.getElementById( "inConstant" ).max = 
+  document.getElementById( "inConstant" ).max = 
     document.getElementById( "maxConstant" ).innerHTML = 
     maxConstant;
 
-    document.getElementById( "inConstant" ).min = 
+  document.getElementById( "inConstant" ).min = 
     document.getElementById( "minConstant" ).innerHTML = 
     minConstant;
 
-    document.getElementById( "inConstant" ).step = stepConstant;
+  document.getElementById( "inConstant" ).step = stepConstant;
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+  gl = WebGLUtils.setupWebGL( canvas );
+  if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    //
-    //  Initialize our data for the Sierpinski Gasket
-    //
+  //
+  //  Initialize our data for the Sierpinski Gasket
+  //
 
-    // First, initialize the corners of our gasket with three points.
-
-
-    //
-    //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
-    // Load the data into the GPU
-
-    bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, 8*Math.pow(4, maxSubdivisions + 1), gl.STATIC_DRAW );
+  // First, initialize the corners of our gasket with three points.
 
 
+  //
+  //  Configure WebGL
+  //
+  gl.viewport( 0, 0, canvas.width, canvas.height );
+  gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
-    // Associate out shader variables with our data buffer
+  //  Load shaders and initialize attribute buffers
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    vColor = gl.getUniformLocation( program, "vColor" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.vertexAttribPointer( vColor, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+  var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+  gl.useProgram( program );
 
-    document.getElementById("inSubdivisions").onchange = 
+  // Load the data into the GPU
+
+  bufferId = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+  gl.bufferData( gl.ARRAY_BUFFER, 8*Math.pow(4, maxSubdivisions + 1), gl.STATIC_DRAW );
+
+
+
+  // Associate out shader variables with our data buffer
+
+  var vPosition = gl.getAttribLocation( program, "vPosition" );
+  vColor = gl.getUniformLocation( program, "vColor" );
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+  gl.vertexAttribPointer( vColor, 2, gl.FLOAT, false, 0, 0 );
+  gl.enableVertexAttribArray( vPosition );
+
+  document.getElementById("inSubdivisions").onchange = 
     document.getElementById("inRotation").onchange = 
     document.getElementById("inConstant").onchange = 
     function() {
@@ -83,15 +84,15 @@ function init()
     };
 
 
-    updateReadouts(getInputs());
-    render({subdivisions: 0, rotation: 0, constant: 0.05});
+  updateReadouts(getInputs());
+  render({subdivisions: 0, rotation: 0, constant: 0.05});
 };
 
 function getInputs(){
   return {
     subdivisions: document.getElementById("inSubdivisions").value, 
-    rotation: document.getElementById("inRotation").value, 
-    constant: document.getElementById("inConstant").value
+      rotation: document.getElementById("inRotation").value, 
+      constant: document.getElementById("inConstant").value
   };
 }
 
@@ -105,75 +106,83 @@ function updateReadouts(inputs)
 
 function triangle( a, b, c )
 {
-    points.push( a, b, c );
+  points.push( a, b, c );
+  lines.push( a, b, b, c, c, a );
 }
 
 function divideTriangle( a, b, c, count, theta, constant )
 {
 
-    // check for end of recursion
+  // check for end of recursion
 
-    if ( count == 0 ) {
-      var points_prime = [];
+  if ( count == 0 ) {
 
-      [a,b,c].forEach(function(point){
-        var x = point[0]; var y = point[1];
-        var d = Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
+    var transformed_points = [a,b,c].map(function(point){
+      var x = point[0]; var y = point[1];
+      var d = Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
 
-        var theta_prime = theta * d * constant;
+      var theta_prime = theta * d * constant;
 
-        var x_prime = x * Math.cos(theta_prime) - y * Math.sin(theta_prime);
-        var y_prime = x * Math.sin(theta_prime) + y * Math.cos(theta_prime);
-        points_prime.push(vec2(x_prime, y_prime));
-      });
+      var x_prime = x * Math.cos(theta_prime) - y * Math.sin(theta_prime);
+      var y_prime = x * Math.sin(theta_prime) + y * Math.cos(theta_prime);
 
-        
-        triangle( points_prime[0], points_prime[1], points_prime[2] );
-    }
-    else {
+      return [x_prime, y_prime]; 
+    });
 
-        //bisect the sides
+    triangle( transformed_points[0], transformed_points[1], transformed_points[2] );
+  }
+  else {
 
-        var ab = mix( a, b, 0.5 );
-        var ac = mix( a, c, 0.5 );
-        var bc = mix( b, c, 0.5 );
+    //bisect the sides
 
-        --count;
+    var ab = mix( a, b, 0.5 );
+    var ac = mix( a, c, 0.5 );
+    var bc = mix( b, c, 0.5 );
 
-        // four new triangles
+    --count;
 
-        divideTriangle( a, ab, ac, count, theta, constant );
-        divideTriangle( c, ac, bc, count, theta, constant );
-        divideTriangle( b, bc, ab, count, theta, constant );
-        divideTriangle( ab, ac, bc, count, theta, constant );
-    }
+    // four new triangles
+
+    divideTriangle( a, ab, ac, count, theta, constant );
+    divideTriangle( c, ac, bc, count, theta, constant );
+    divideTriangle( b, bc, ab, count, theta, constant );
+    divideTriangle( ab, ac, bc, count, theta, constant );
+  }
 }
 
 window.onload = init;
 
 function render(inputs)
 {
-    var vertices = [
-        vec2( -1, -1 ),
-        vec2(  0,  1 ),
-        vec2(  1, -1 )
-    ];
-    points = [];
-    divideTriangle( vertices[0], vertices[1], vertices[2],
-                    inputs.subdivisions, inputs.rotation, inputs.constant);
+  var vertices = [
+    [ -1, -1 ],
+    [  0,  1 ],
+    [  1, -1 ]
+      ];
+  points = []; lines = [];
+  divideTriangle( vertices[0], vertices[1], vertices[2],
+      inputs.subdivisions, inputs.rotation, inputs.constant);
 
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    // TODO: also render outlines of triangles
-    gl.uniform4f(vColor, 1, 0, 0, 1);
-    gl.drawArrays( gl.TRIANGLES, 0, points.length );
-    gl.uniform4f(vColor, 0, 0, 0, 1);
+  gl.clear( gl.COLOR_BUFFER_BIT );
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
 
-    for(var ix = 0; ix < points.length; ix += 3)
-    {
-      gl.drawArrays( gl.LINE_LOOP, ix, 3 );
-    }
-    // TODO: time function and compare with gpu sin/cos processing
-    points = [];
-    //requestAnimFrame(render);
+  gl.uniform4f(vColor, 1, 0, 0, 1);
+  gl.drawArrays( gl.TRIANGLES, 0, points.length );
+
+  // TODO: figure out how to do this more efficiently.
+  // likely best bet would be to construct another buffer and pass it to gl object
+  // to use with line strip/loop in one pass
+  gl.uniform4f(vColor, 0, 0, 0, 1);
+  /*
+  for(var ix = 0; ix < points.length; ix += 3)
+  {
+    gl.drawArrays( gl.LINE_LOOP, ix, 3 );
+  }
+  */
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(lines));
+  gl.drawArrays( gl.LINES, 0, lines.length );
+
+  // TODO: time function and compare with gpu sin/cos processing
+  points = [];
+  //requestAnimFrame(render);
 }
