c43aa7d47caf62ed02dd116c276bd1e67593bc48
NetsydeMiro <miro@netsyde.com>
2015-07-27 13:31:44 -0400

    rotation projection changes; not working yet

--- assignments/simple_cad/simpleCad.js
+++ assignments/simple_cad/simpleCad.js
@@ -11,9 +11,11 @@ var pointsArray = [];
 
 var near = -10;
 var far = 10;
+/*
 var radius = 6.0;
 var theta  = 0.0;
 var phi    = 0.0;
+*/
 var dr = 5.0 * Math.PI/180.0;
 
 var left = -2.0;
@@ -26,7 +28,8 @@ var modelViewMatrixLoc, projectionMatrixLoc, colorLoc;
 var eye;
 
 var position = {x: 0, y: 0};
-var scale = {x: 1, y: 1};
+var scale = {x: 1, y: 1, z: 1};
+var rotation = {x: 0, y:0, z:0};
 
 var init;
 
@@ -92,13 +95,15 @@ simpleCad.directive('rangeSelector', function() {
 simpleCad.controller('SimpleCadController', ['$scope', function($scope){
 
   $scope.sliders = {
-    subdivisions: {label: 'Subdivisions',       min: 0, max: 6,    step: 1,  value: 3}, 
-    x:            {label: 'X',                  min: -1, max: 1,    step: 0.01,  value: 0}, 
-    y:            {label: 'Y',                  min: -1, max: 1,    step: 0.01,  value: 0}, 
-    scaleX:       {label: 'Scale X',            min: -1, max: 1,    step: 0.01,  value: 0.5}, 
-    scaleY:       {label: 'Scale Y',            min: -1, max: 1,    step: 0.01,  value: 0.5}, 
-    theta:        {label: 'Rotation (&Theta;)', min: 0, max: 360,  step: 1,  value: 0}, 
-    phi:          {label: 'Rotation (&Phi;)',   min: 0, max: 360,  step: 1,  value: 0}
+    subdivisions: {label: 'Subdivisions', min: 0, max: 6,  step: 1,     value: 3}, 
+    positionX:    {label: 'Position X',   min: -1, max: 1, step: 0.01,  value: 0}, 
+    positionY:    {label: 'Position Y',   min: -1, max: 1, step: 0.01,  value: 0}, 
+    scaleX:       {label: 'Scale X',      min: -1, max: 1, step: 0.01,  value: 0.5}, 
+    scaleY:       {label: 'Scale Y',      min: -1, max: 1, step: 0.01,  value: 0.5}, 
+    scaleZ:       {label: 'Scale Z',      min: -1, max: 1, step: 0.01,  value: 0.5}, 
+    rotateX:      {label: 'Rotation X', min: 0, max: 360,  step: 1,     value: 0}, 
+    rotateY:      {label: 'Rotation Y', min: 0, max: 360,  step: 1,     value: 0}, 
+    rotateZ:      {label: 'Rotation Z', min: 0, max: 360,  step: 1,     value: 0}
   };
 
   var getInputs = function(){
@@ -113,11 +118,14 @@ simpleCad.controller('SimpleCadController', ['$scope', function($scope){
     if (init)
     {
       numTimesToSubdivide = newVal.subdivisions;
+      /*
       theta = Math.PI * 2 / 360 * newVal.theta;
       phi = Math.PI * 2 / 360 * newVal.phi;
+      */
 
-      position = {x: newVal.x, y: newVal.y};
+      position = {x: newVal.positionX, y: newVal.positionY};
       scale = {x: newVal.scaleX, y: newVal.scaleY, z: newVal.scaleZ};
+      rotation = {x: newVal.rotationX, y: newVal.rotationY, z: newVal.rotationZ};
 
       if (newVal.subdivions != oldVal.subdivisions)
       {
@@ -170,11 +178,10 @@ simpleCad.controller('SimpleCadController', ['$scope', function($scope){
       modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
       projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
       colorLoc = gl.getUniformLocation( program, "color" );
-
-      render();
     };
 
     init();
+    render();
   });
 }]);
 
@@ -183,11 +190,29 @@ function render() {
 
   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+  /*
   eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
       radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+      */
+
+  eye = vec3(0,0,1);
 
   modelViewMatrix = lookAt(eye, at , up);
-  projectionMatrix = mult(translate(position.x,position.y,0), mult(scalem(scale.x,scale.y,1), ortho(left, right, bottom, ytop, near, far)));
+
+  //projectionMatrix = scalem(scale.x,scale.y,scale.z);
+
+  projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+  projectionMatrix = mult(scalem(scale.x, scale.y, scale.z), projectionMatrix);
+
+  /*
+  projectionMatrix = mult(rotate(rotate.x, [1,0,0]), projectionMatrix);
+  projectionMatrix = mult(rotate(rotate.y, [0,1,0]), projectionMatrix);
+  projectionMatrix = mult(rotate(rotate.z, [0,0,1]), projectionMatrix);
+  */
+
+  projectionMatrix = mult(translate(position.x, position.y), projectionMatrix);
+
+  //projectionMatrix = mult(ortho(left, right, bottom, ytop, near, far), projectionMatrix);
 
   gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
   gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
